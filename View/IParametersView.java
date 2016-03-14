53ca70fbad491b7b7fe938b1f95e4c254c8df232
danyp <danypatient@gmail.com>
2016-03-14 14:33:24 +0100

    create GameController, IGameView, GameView and IParametersView

---
+++ View/IParametersView.java
@@ -0,0 +1,20 @@
+package View;
+
+/**
+ * Created by DanyPatient on 14/03/2016.
+ */
+public interface IParametersView {
+    void speedBall();
+
+    void spawnIntervalBall();
+
+    void drawParameters();
+
+    void drawLogo();
+
+    void drawButtonStart();
+
+    void drawSetterSpeedBall();
+
+    void drawSetterIntervalBall();
+}
