99e2244b0ef32e9078ad424f729b6ca5bdaa46f6
ZwoRmi <lemaire.lucas1@gmail.com>
2016-03-29 10:00:23 +0200

    Tuple tests

--- src/test/java/Model/TupleTest.java
+++ src/test/java/Model/TupleTest.java
@@ -4,19 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Lucas on 29/03/2016.
- */
 public class TupleTest {
     @Test
     public void testControllerInitX() throws Exception {
-        Tuple<Integer, Double> target = new Tuple<>(Integer.valueOf(0), Double.valueOf(2));
+        Tuple<Integer, Double> target = new Tuple<>(0, 2d);
         assertEquals(Integer.valueOf(0), target.x);
     }
-    
+
     @Test
     public void testControllerInitY() throws Exception {
-        Tuple<Integer, Double> target = new Tuple<>(Integer.valueOf(0), Double.valueOf(2));
+        Tuple<Integer, Double> target = new Tuple<>(0, 2d);
         assertEquals(Double.valueOf(2), target.y);
     }
 }
